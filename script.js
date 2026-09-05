67a502e7020f1573d87e96f3b7aa1e42edc22403
omar1954 <279541643+omar1954@users.noreply.github.com>
2026-09-05 19:43:52 -0400

    Refactor video play logic and clean up code

--- script.js
+++ script.js
@@ -32,14 +32,9 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     playBig.addEventListener('click', function () {
       if (mainVideo.paused || mainVideo.ended) {
-       if (mainVideo.ended || mainVideo.currentTime >= mainVideo.duration - 0.5) {
+        if (mainVideo.ended || mainVideo.currentTime >= mainVideo.duration - 0.5) {
           mainVideo.load();
           mainVideo.play();
-        } else {
-          mainVideo.play();
-        };
-          mainVideo.addEventListener('seeked', playAfterSeek);
-          mainVideo.currentTime = 0;
         } else {
           mainVideo.play();
         }
